5288386e3eb4a8acb7ff4b35c6d9bc874bc73991
Charles-Antoine Mathieu <skatkatt@root.gg>
2015-12-28 23:21:53 +0100

    save

--- juliet_test.go
+++ juliet_test.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 	"fmt"
 	"testing"
+//	"github.com/gorilla/mux"
+	"net/http/httptest"
+	"log"
+	"io/ioutil"
 	"github.com/gorilla/mux"
 )
 
@@ -17,6 +21,7 @@ func appMiddleware1(ctx *Context, next http.Handler) http.Handler {
 
 func appMiddleware2(ctx *Context, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request){
+		fmt.Printf("vars %v",mux.Vars(req))
 		fmt.Println("app middlerware2")
 		ctx.Set("value",2)
 		next.ServeHTTP(resp,req)
@@ -42,23 +47,38 @@ func appHandlerFunc(resp http.ResponseWriter, req *http.Request){
 	resp.Write([]byte("yo"))
 }
 
+func serveAndRequest(h http.Handler) string {
+	ts := httptest.NewServer(h)
+	defer ts.Close()
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resBody, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(resBody)
+}
+
 func Test(t *testing.T){
 	stack := NewChain(appMiddleware1)
 	stack2 := stack.Append(appMiddleware2)
-	stack3 := stack2.Append(appMiddleware3)
+//	stack3 := stack2.Append(appMiddleware3)
 
-	stack4 := NewChain(appMiddleware2).AppendChain(stack3)
-	stack5 := stack3.AppendChain(stack3)
-
+//	stack4 := NewChain(appMiddleware2).AppendChain(stack3)
+//	stack5 := stack3.AppendChain(stack3)
+//
 	r := mux.NewRouter()
-	r.Handle("/1", stack.Then(appHandler))
-	r.Handle("/2", stack2.Then(appHandler))
-	r.Handle("/3", stack3.Then(appHandler))
-	r.Handle("/4", stack4.Then(appHandler))
-	r.Handle("/5", stack5.Then(appHandler))
-	r.Handle("/6", stack5.ThenHandlerFunc(appHandlerFunc))
-	r.Handle("/7", stack5.ThenHandler(http.NotFoundHandler()))
-	r.Handle("/8/{id}", stack5.ThenHandler(http.NotFoundHandler()))
-//	http.Handle("/", appMiddleware(http.HandlerFunc(appHandler)))
+//	fmt.Println(serveAndRequest(stack.Then(appHandler)))
+	r.Handle("/2/{id}", stack2.Then(appHandler))
+//	r.Handle("/3", stack3.Then(appHandler))
+//	r.Handle("/4", stack4.Then(appHandler))
+//	r.Handle("/5", stack5.Then(appHandler))
+//	r.Handle("/6", stack5.ThenHandlerFunc(appHandlerFunc))
+//	r.Handle("/7", stack5.ThenHandler(http.NotFoundHandler()))
+//	r.Handle("/8/{id}", stack5.ThenHandler(http.NotFoundHandler()))
+////	http.Handle("/", appMiddleware(http.HandlerFunc(appHandler)))
 	http.ListenAndServe(":3000",r)
 }
